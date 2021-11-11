d075143a5bb8f6e195fbc8fdf828b23e6a7e2dd4
Lance Pidor <kyrios213@gmail.com>
2021-11-11 12:43:32 +0800

    FInal commit

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,16 +1,23 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
     <label>Email:</label>
     <input type="email" required v-model="email" />
 
     <label>Password:</label>
     <input type="password" required v-model="password" />
+    <div v-if="passwordError" class="error">{{ passwordError }}</div>
 
     <label>Role: </label>
     <select v-model="role">
       <option value="developer">Web Developer</option>
       <option value="designer">Web Designer</option>
     </select>
+
+    <label>Skills:</label>
+    <input type="text" v-model="tempSkill" @keyup.alt="addSkill" />
+    <div v-for="skill in skills" :key="skill" class="pill">
+      <span @click="removeSkill(skill)">{{ skill }}</span>
+    </div>
 
     <div class="terms">
       <input type="checkbox" required v-model="terms" />
@@ -21,7 +28,7 @@
       <input type="checkbox" value="shaun" v-model="names" />
       <label>Shaun</label>
     </div>
-    <div>
+    <div
       <input type="checkbox" value="yoshi" v-model="names" />
       <label>Yoshi</label>
     </div>
@@ -29,6 +36,10 @@
       <input type="checkbox" value="mario" v-model="names" />
       <label>Mario</label>
     </div> -->
+
+    <div class="submit">
+      <button>Sign Up</button>
+    </div>
   </form>
 
   <p>Email: {{ email }}</p>
@@ -47,8 +58,35 @@ export default {
       password: "",
       role: "developer",
       terms: false,
+      tempSkill: "",
+      skills: [],
+      passwordError: "",
       //   names: [],
     };
+  },
+  methods: {
+    addSkill(e) {
+      if (e.key === "," && this.tempSkill) {
+        if (!this.skills.includes(this.tempSkill)) {
+          this.skills.push(this.tempSkill);
+        }
+        this.tempSkill = "";
+      }
+    },
+    removeSkill(skill) {
+      this.skills = this.skills.filter((item) => item != skill);
+    },
+    handleSubmit() {
+      // validate password
+      this.passwordError =
+        this.password.length > 5
+          ? ""
+          : "Password must be at least 6 characters long";
+
+      if (!this.passwordError) {
+        console.log("Form submitted");
+      }
+    },
   },
 };
 </script>
@@ -87,5 +125,34 @@ input[type="checkbox"] {
   margin: 0 10px 0 0;
   position: relative;
   top: 2px;
+}
+.pill {
+  display: inline-block;
+  margin: 20px 10px 0 0;
+  padding: 6px 12px;
+  background: #eee;
+  border-radius: 20px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: #777;
+  cursor: pointer;
+}
+button {
+  background: #9b5dff;
+  border: 0;
+  padding: 10px 20px;
+  margin-top: 20px;
+  color: white;
+  border-radius: 20px;
+}
+.submit {
+  text-align: center;
+}
+.error {
+  color: #ff0062;
+  margin-top: 10px;
+  font-size: 0.8em;
+  font-weight: bold;
 }
 </style>
